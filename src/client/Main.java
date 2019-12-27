a81515de492bb4dbddf4d653ddfc346b98c55c14
kirill <thegaldren@gmail.com>
2019-12-27 13:00:16 +0300

    encapsulate inputHandler and check filename

--- src/client/Main.java
+++ src/client/Main.java
@@ -1,5 +1,6 @@
 package client;
 
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
@@ -7,26 +8,50 @@ public class Main {
         HttpClient httpClient = new HttpClient(8080);
         httpClient.connectToServer();
 
+        handleInput(httpClient);
+    }
+
+    private static void handleInput(HttpClient httpClient) {
+        System.out.println("\nYou can use POST [filename], GET [filename] and STOP commands\n");
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("\nYou can use POST [filename], GET [filename] and STOP commands\n");
-
-        whileLoop: while (true){ // TODO maybe encapsulate that while?
+        while (true){
             String[] input = scanner.nextLine().split(" ");
             switch (input[0].toUpperCase()){    // TODO enum with keys?
                 case "POST":
-                    httpClient.sendPostRequest(input[1]); // TODO check bounds
+                    if(filenameIsFine(input)){
+                        httpClient.sendPostRequest(input[1]);
+                    }
                     break;
                 case "GET":
-                    httpClient.sendGetRequest(input[1]);
+                    if(filenameIsFine(input)){
+                        httpClient.sendGetRequest(input[1]);
+                    }
                     break;
                 case "STOP":
                     httpClient.stop();
-                    break whileLoop;
+                    return;
                 default:
-                    System.out.println("Wrong input");
+                    System.out.println("Wrong command");
+            }
+        }
+    }
+
+    private static boolean filenameIsFine(String[] input) {
+
+        if(input.length < 2){
+            System.out.println("No filename");
+            return false;
+        }
+
+        if(input[0].toUpperCase().equals("POST")){
+            File file = new File(input[1]);
+            if(!file.exists()){
+                System.out.println("File doesn't exist");
+                return false;
             }
         }
 
+        return true;
     }
 }
