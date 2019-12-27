307de5fec4d3cd2092cb7b45ddd1a7f9d909f32a
kirill <thegaldren@gmail.com>
2019-12-27 09:02:53 +0300

    server: get data from client and send date

--- src/server/HttpServer.java
+++ src/server/HttpServer.java
@@ -1,5 +1,12 @@
 package server;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+
 public class HttpServer {
 
     private final String address;
@@ -11,8 +18,36 @@ public class HttpServer {
     }
 
     public void run(){
-        System.out.println("Server started, but that ain't actually true");
         // TODO start a thread which checks input connections
+
+        try(ServerSocket serverSocket = new ServerSocket(port)){
+            System.out.println("Server started, but you can't stop it");
+
+            while (true){
+
+                try(Socket clientSocket = serverSocket.accept();
+                    InputStreamReader isr = new InputStreamReader(clientSocket.getInputStream());
+                    BufferedReader reader = new BufferedReader(isr)){                               // TODO add output stream
+
+                    System.out.println("\nClient sent: \n");
+
+                    String line = reader.readLine();
+                    while (!line.isEmpty()){
+                        System.out.println(line);
+                        line = reader.readLine();
+                    }
+
+                    String httpResponse = "HTTP/1.1 200 OK\r\n\r\n" + new Date();
+
+                    System.out.println("\nWe are sending: \n" + httpResponse);
+
+                    clientSocket.getOutputStream().write(httpResponse.getBytes());
+                }
+            }
+
+        } catch (IOException e){
+            e.printStackTrace();
+        }
     }
 
     public void stop(){
