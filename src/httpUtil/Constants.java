8b5f6a54d6f78fda2c604173b17ee0be4930235a
kirill <thegaldren@gmail.com>
2019-12-27 14:01:36 +0300

    add Constants

---
+++ src/httpUtil/Constants.java
@@ -0,0 +1,11 @@
+package httpUtil;
+
+public interface Constants {
+    String POST = "POST";
+    String GET = "GET";
+    String HEAD = "HEAD";
+
+    String STOP = "STOP";
+
+    String CONTENT_LENGTH = "Content-Length:";
+}
