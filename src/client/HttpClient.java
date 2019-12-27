e0af0606c20305122897af6b594377b894b1f7a6
kirill <thegaldren@gmail.com>
2019-12-27 17:15:05 +0300

    sendPostRequest actually sends a request

--- src/client/HttpClient.java
+++ src/client/HttpClient.java
@@ -1,13 +1,18 @@
 package client;
 
-import java.io.IOException;
+import httpUtil.HttpRequestParser;
+
+import java.io.*;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.nio.file.Files;
 
 public class HttpClient {
     private final int port;
 
     private Socket socket;
+    private OutputStream outputStream;
+    private BufferedReader bufferedReader;
 
     HttpClient(int port){
         this.port = port;
@@ -17,14 +22,34 @@ public class HttpClient {
         System.out.println("Connecting server with port: " + port);
         try{
             socket = new Socket(InetAddress.getByName(null), port);
+
+            InputStream inputStream = socket.getInputStream();
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            bufferedReader = new BufferedReader(inputStreamReader);
+
+            outputStream = socket.getOutputStream();
+
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     public void sendPostRequest(String filename){
-        System.out.println("Should send post request for " + filename);
-        // TODO
+
+        File file = new File(filename);
+
+        String header = HttpRequestParser.preparePostRequestHeader(filename, file.length());
+
+        System.out.println("\nSending\n" + header);
+
+        try {
+            outputStream.write(header.getBytes());
+            Files.copy(file.toPath(), outputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // TODO read response
     }
 
     public void sendGetRequest(String filename){
