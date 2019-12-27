821bb25ba5d39912e95df37988bd94d4f61f5bba
kirill <thegaldren@gmail.com>
2019-12-27 15:55:42 +0300

    add comments to Constants

--- src/httpUtil/Constants.java
+++ src/httpUtil/Constants.java
@@ -1,11 +1,15 @@
 package httpUtil;
 
 public interface Constants {
+
+    // HTTP methods, also used as client commands
     String POST = "POST";
     String GET = "GET";
     String HEAD = "HEAD";
 
+    // Other client commands
     String STOP = "STOP";
 
+    // HTTP fields
     String CONTENT_LENGTH = "Content-Length:";
 }
