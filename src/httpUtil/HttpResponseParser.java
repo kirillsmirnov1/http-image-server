2bd8025f2aff2ac45d953800af96f1628979a1de
kirill <thegaldren@gmail.com>
2019-12-28 09:32:57 +0300

    add HttpResponseParser.parseResponse

--- src/httpUtil/HttpResponseParser.java
+++ src/httpUtil/HttpResponseParser.java
@@ -1,10 +1,46 @@
 package httpUtil;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+
 import static httpUtil.Constants.CONTENT_LENGTH;
 import static httpUtil.Constants.HTTP_1_1;
 
 public class HttpResponseParser {
 
+    public static HttpResponseHeader parseResponse(BufferedReader reader){
+
+        try {
+            HttpResponseHeader header = new HttpResponseHeader();
+            StringBuilder headerContents = new StringBuilder();
+
+            String line = reader.readLine();
+
+            while (!line.isEmpty()){
+                headerContents.append(line).append("\n");
+                String[] tockens = line.split(" ");
+
+                switch (tockens[0]){
+                    case HTTP_1_1:
+                        header.setCode(tockens[1]);
+                        break;
+                    case CONTENT_LENGTH:
+                        header.setContentLength(Integer.parseInt(tockens[1]));
+                        break;
+                }
+
+                line = reader.readLine();
+            }
+
+            header.setHeaderContents(headerContents.toString());
+
+            return header;
+
+        } catch (IOException e) {
+            System.out.println("Couldn't read response header");
+        }
+        return null;
+    }
 
     public static String prepareResponseHeader(String statusCode) {
         return prepareResponseHeader(statusCode, -1);
