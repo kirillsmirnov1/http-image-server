a78e1cdbe8ca69c6e33eea6f7970892029aee1da
kirill <thegaldren@gmail.com>
2019-12-28 13:17:36 +0300

    make HttpRequestParser inner class of ServerToClientConnection

--- src/server/ServerToClientConnection.java
+++ src/server/ServerToClientConnection.java
@@ -1,10 +1,11 @@
 package server;
 
+import httpUtil.HttpMethod;
 import httpUtil.HttpRequestHeader;
-import httpUtil.HttpRequestParser;
 
 import java.io.*;
 import java.net.Socket;
+import java.net.SocketException;
 import java.nio.file.Files;
 
 import static httpUtil.Constants.*;
@@ -35,7 +36,7 @@ public class ServerToClientConnection implements Runnable {
 
             while(keepConnectionToAClient) {
 
-                HttpRequestHeader header = HttpRequestParser.parseRequest(reader);
+                HttpRequestHeader header = new HttpRequestParser().parseRequest(reader);
 
                 if(header == null){
                     break;
@@ -150,5 +151,43 @@ public class ServerToClientConnection implements Runnable {
 
     public void timeToCloseConnection(){
         closeConnection(); // FIXME check if transaction is in work
+    }
+
+    private class HttpRequestParser {
+
+        public HttpRequestHeader parseRequest(BufferedReader reader) {
+            try {
+                HttpRequestHeader header = new HttpRequestHeader(null, null, -1);
+
+                String line = reader.readLine();
+
+                while(!line.isEmpty()){
+                    String[] tokens = line.split(" ");
+
+                    switch (tokens[0]){
+                        case POST: // FIXME it would be better to use enum here, but POST.name() doesn't work here and I couldn't think of anything else
+                        case GET:
+                        case HEAD: {
+                            header.setMethod(HttpMethod.valueOf(tokens[0]));
+
+                            // Implying that we are storing all files in server root
+                            header.setFileName(tokens[1].substring(tokens[1].lastIndexOf("/") + 1)); // TODO might need to check if there is actually something in there
+                            break;
+                        }
+                        case CONTENT_LENGTH: {
+                            header.setContentLength(Integer.parseInt(tokens[1]));
+                        }
+                    }
+                    line = reader.readLine();
+                }
+
+                return header;
+            } catch (SocketException | NullPointerException e){
+                System.out.println("Connection to client seems to be closed");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
     }
 }
